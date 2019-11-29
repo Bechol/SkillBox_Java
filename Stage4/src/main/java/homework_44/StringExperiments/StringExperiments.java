ecf8706d29722cb7d5895091af58bfab8a119f49
Bechol <oleg071984@gmail.com>
2019-11-29 17:02:19 +0300

    переделал 4.4.2 с испольованием методов indexOf(), lastIndexOf(), substring() и trim()

--- Stage4/src/main/java/homework_44/StringExperiments/StringExperiments.java
+++ Stage4/src/main/java/homework_44/StringExperiments/StringExperiments.java
@@ -1,23 +1,13 @@
 package homework_44.StringExperiments;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 /**
  * 4.4.2
  */
 public class StringExperiments {
     public static void main(String[] args) {
         String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-        String[] arr = text.split(", ");
-        int sum = 0;
-        for (String str : arr) {
-            Pattern pattern = Pattern.compile("\\d+");
-            Matcher matcher = pattern.matcher(str);
-            if (str.contains("Вася") || str.contains("Маша"))
-                if (matcher.find())
-                    sum += Integer.valueOf(str.substring(matcher.start(), matcher.end()));
-        }
-        System.out.println(sum);
+        int vasiaSalary = Integer.valueOf(text.substring(text.indexOf("5000"), text.indexOf("рублей")).trim());
+        int mashaSalary = Integer.valueOf(text.substring(text.indexOf("30000"), text.lastIndexOf("рублей")).trim());
+        System.out.println("Сумма заработка Васи и Маши: " + (vasiaSalary + mashaSalary));
     }
 }
