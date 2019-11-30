a3955b44d5e2491b0c91bb43373659844ce3ca60
Oleg <37021780+Bechol@users.noreply.github.com>
2019-11-30 10:02:19 +0300

    корректировка по замечаниям IDEA

--- Stage4/src/main/java/homework_44/StringExperiments/StringExperiments.java
+++ Stage4/src/main/java/homework_44/StringExperiments/StringExperiments.java
@@ -6,8 +6,8 @@ package homework_44.StringExperiments;
 public class StringExperiments {
     public static void main(String[] args) {
         String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-        int vasiaSalary = Integer.valueOf(text.substring(text.indexOf("5000"), text.indexOf("рублей")).trim());
-        int mashaSalary = Integer.valueOf(text.substring(text.indexOf("30000"), text.lastIndexOf("рублей")).trim());
+        int vasiaSalary = Integer.parseInt(text.substring(text.indexOf("5000"), text.indexOf("рублей")).trim());
+        int mashaSalary = Integer.parseInt(text.substring(text.indexOf("30000"), text.lastIndexOf("рублей")).trim());
         System.out.println("Сумма заработка Васи и Маши: " + (vasiaSalary + mashaSalary));
     }
 }
