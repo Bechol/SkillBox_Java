78f375110e489c087b60e296671cd1bd8858f31e
Oleg <37021780+Bechol@users.noreply.github.com>
2019-12-06 01:10:08 +0300

    Перенес искомые значения в переменные

--- Stage4/src/main/java/homework_44/StringExperiments/StringExperiments.java
+++ Stage4/src/main/java/homework_44/StringExperiments/StringExperiments.java
@@ -4,9 +4,11 @@ package homework_44.StringExperiments;
  * 4.4.2
  */
 public class StringExperiments {
+    private static String first = "5000";
+    private static String second = "30000";
     public static void main(String[] args) {
         String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-        int vasiaSalary = Integer.parseInt(text.substring(text.indexOf("5000"), text.indexOf("рублей")).trim());
+        int vasiaSalary = Integer.parseInt(text.substring(text.indexOf(first), text.indexOf(second)).trim());
         int mashaSalary = Integer.parseInt(text.substring(text.indexOf("30000"), text.lastIndexOf("рублей")).trim());
         System.out.println("Сумма заработка Васи и Маши: " + (vasiaSalary + mashaSalary));
     }
