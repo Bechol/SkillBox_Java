578b46c2372214475631fd30eb2ce2c8fe61de65
Oleg <37021780+Bechol@users.noreply.github.com>
2019-12-06 01:05:30 +0300

    Реализация с использованием Calendar и SimpleDateFormat

--- Stage4/src/main/java/homework_46/BirthDay/Main.java
+++ Stage4/src/main/java/homework_46/BirthDay/Main.java
@@ -1,27 +1,21 @@
 package homework_46.BirthDay;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.time.format.DateTimeFormatter;
-import java.time.format.FormatStyle;
-import java.time.format.TextStyle;
-import java.util.Locale;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 /**
  * 4.6
  */
 public class Main {
 
-    private static LocalDate startDate = LocalDate.of(1984, Month.JULY, 11); //мой день рождения
-    private static DateTimeFormatter shortDateTime = DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT);
+    private static Calendar myBirthDate = Calendar.getInstance();
 
     public static void main(String[] args) {
-        int age = 0;
-        while (startDate.isBefore(LocalDate.now())) {
-            System.out.println(age + " - " + startDate.format(shortDateTime) + " - "
-                    + startDate.getDayOfWeek().getDisplayName(TextStyle.SHORT, Locale.getDefault()));
-            startDate = startDate.plusYears(1);
-            age++;
+        myBirthDate.set(1984, Calendar.JULY, 11);
+        for (int i = 0; myBirthDate.getTime().before(Calendar.getInstance().getTime()); i++) {
+            System.out.println(i + " - " + new SimpleDateFormat("d.MM.y '-' E").format(myBirthDate.getTime()));
+            myBirthDate.set(Calendar.YEAR, myBirthDate.get(Calendar.YEAR) + 1);
         }
     }
 }
