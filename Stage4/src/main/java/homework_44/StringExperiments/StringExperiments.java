9584c423ca481cb9bd456ecd81fac8f7ab20e239
Oleg <37021780+Bechol@users.noreply.github.com>
2019-12-07 16:28:15 +0300

    перенес переменную second

--- Stage4/src/main/java/homework_44/StringExperiments/StringExperiments.java
+++ Stage4/src/main/java/homework_44/StringExperiments/StringExperiments.java
@@ -8,8 +8,8 @@ public class StringExperiments {
     private static String second = "30000";
     public static void main(String[] args) {
         String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-        int vasiaSalary = Integer.parseInt(text.substring(text.indexOf(first), text.indexOf(second)).trim());
-        int mashaSalary = Integer.parseInt(text.substring(text.indexOf("30000"), text.lastIndexOf("рублей")).trim());
+        int vasiaSalary = Integer.parseInt(text.substring(text.indexOf(first), text.indexOf("рублей")).trim());
+        int mashaSalary = Integer.parseInt(text.substring(text.indexOf(second), text.lastIndexOf("рублей")).trim());
         System.out.println("Сумма заработка Васи и Маши: " + (vasiaSalary + mashaSalary));
     }
 }
