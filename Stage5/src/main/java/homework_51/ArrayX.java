ee4c871dda057215eefbe3795e1ab626d93da551
Oleg <37021780+Bechol@users.noreply.github.com>
2019-12-08 16:24:09 +0300

    выполнил 5.1.3

---
+++ Stage5/src/main/java/homework_51/ArrayX.java
@@ -0,0 +1,26 @@
+package homework_51;
+
+/**
+ * 5.1.3
+ *
+ * @see package-info.java
+ */
+public class ArrayX {
+    private static final int SQUARE_SIZE = 7;
+
+    public static void main(String[] args) {
+        String[][] arr = new String[SQUARE_SIZE][SQUARE_SIZE];
+        for (int i = 0; i < SQUARE_SIZE; i++) {
+            for (int j = 0; j < SQUARE_SIZE; j++) {
+                if (i == j || (i + j + 1) == SQUARE_SIZE) {
+                    arr[i][j] = "x";
+                } else {
+                    arr[i][j] = " ";
+                }
+                System.out.print(arr[i][j]);
+
+            }
+            System.out.println("\n");
+        }
+    }
+}
