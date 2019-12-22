e43bab4c2d9dc9ae341e0e9fdf4cf1b80fad7435
Oleg <37021780+Bechol@users.noreply.github.com>
2019-12-22 11:40:33 +0300

    5.5. Добавил генерацию номеров для всех возможных комбинаций с одинаковыми цифрами в номере.

--- Stage5/src/main/java/homework_55/NumberGenerator.java
+++ Stage5/src/main/java/homework_55/NumberGenerator.java
@@ -17,7 +17,11 @@ public class NumberGenerator {
     private final static String[] NUMBER_CHARS = new String[]{"C", "M", "T", "B", "A", "P", "O", "H", "E", "У"};
 
     public static void main(String[] args) {
+        System.out.println("Формирование списка номеров..");
         numbers = numbersGemerator();
+        System.out.println("Список номеров сформирован. Кол-во уникальных номеров: " + numbers.size());
+
+        System.out.println(numbers.size());
         while (true) {
             System.out.println("Введите номер автомобиля:");
             String searchNumber = new Scanner(System.in).nextLine();
@@ -25,22 +29,20 @@ public class NumberGenerator {
             bruteForceSearch(searchNumber);
             binarySearch(searchNumber);
 
-            TreeSet<String> numbersTreeSet = new TreeSet<>();
-            numbersTreeSet.addAll(numbers);
+            TreeSet<String> numbersTreeSet = new TreeSet<>(numbers);
             System.out.println("=======TreeSet======");
-            long start = System.currentTimeMillis();
+            long start = System.nanoTime();
             if (numbersTreeSet.contains(searchNumber)) {
-                System.out.println("Номер найден. Время поиска: " + (System.currentTimeMillis() - start) + "мс");
+                System.out.println("Номер найден. Время поиска: " + (System.nanoTime() - start) + "нс");
             } else {
                 System.out.println("Номер не найден");
             }
 
-            HashSet<String> numbersHashSet = new HashSet<>();
-            numbersHashSet.addAll(numbers);
+            HashSet<String> numbersHashSet = new HashSet<>(numbers);
             System.out.println("=======HashSet======");
-            long start1 = System.currentTimeMillis();
+            long start1 = System.nanoTime();
             if (numbersHashSet.contains(searchNumber)) {
-                System.out.println("Номер найден. Время поиска: " + (System.currentTimeMillis() - start1) + "мс");
+                System.out.println("Номер найден. Время поиска: " + (System.nanoTime() - start1) + "нс");
             } else {
                 System.out.println("Номер не найден");
             }
@@ -57,12 +59,16 @@ public class NumberGenerator {
         int regionCode = 1;
         List<String> result = new ArrayList<>();
         while (regionCode < 200) {
-            for (String aChar : NUMBER_CHARS) {
-                for (int j = 1; j < 1000; j++) {
-                    if (regionCode < 100) {
-                        result.add(String.format("%s%03d%s%s%02d", aChar, j, aChar, aChar, regionCode));
-                    } else {
-                        result.add(String.format("%s%03d%s%s%03d", aChar, j, aChar, aChar, regionCode));
+            for (String x : NUMBER_CHARS) {
+                for (String y : NUMBER_CHARS) {
+                    for (String z : NUMBER_CHARS) {
+                        for (int j = 1; j < 1000; j++) {
+                            if (regionCode < 100) {
+                                result.add(String.format("%s%03d%s%s%02d", x, j, y, z, regionCode));
+                            } else {
+                                result.add(String.format("%s%03d%s%s%03d", x, j, y, z, regionCode));
+                            }
+                        }
                     }
                 }
             }
@@ -73,10 +79,10 @@ public class NumberGenerator {
 
     private static void bruteForceSearch(String myNumber) {
         System.out.println("=======Поиск перебором======");
-        long start = System.currentTimeMillis();
+        long start = System.nanoTime();
         for (String number : numbers) {
             if (number.equals(myNumber)) {
-                System.out.println("Номер найден. Время поиска: " + (System.currentTimeMillis() - start) + "мс");
+                System.out.println("Номер найден. Время поиска: " + (System.nanoTime() - start) + "нс");
                 break;
             }
         }
@@ -86,10 +92,10 @@ public class NumberGenerator {
     private static void binarySearch(String myNumber) {
         System.out.println("=======Двоичный поиск======");
         Collections.sort(numbers);
-        long start = System.currentTimeMillis();
+        long start = System.nanoTime();
         long result = Collections.binarySearch(numbers, myNumber);
         if (result != -1) {
-            System.out.println("Номер найден. Время поиска: " + (System.currentTimeMillis() - start) + "мс");
+            System.out.println("Номер найден. Время поиска: " + (System.nanoTime() - start) + "нс");
         } else {
             System.out.println("Номер не найден.");
         }
