c69dbb939f5fee476dda4802c577169695f4b91a
Oleg <37021780+Bechol@users.noreply.github.com>
2020-01-14 01:08:37 +0300

    Выполнил 9.7

---
+++ Stage9/src/main/java/FolderCopy/Main.java
@@ -0,0 +1,30 @@
+package FolderCopy;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
+
+
+/**
+ * Домашняя работа 9.7
+ * Написать код, который будет копировать указанную папку с файлами с сохранением структуры в другую указанную папку.
+ *
+ * @author Oleg Bech
+ * @email oleg071984@gmail.com
+ */
+public class Main {
+
+    private static final File SRC_DIR = new File("Stage9/src/main/java/FolderCopy/primer/src_dir");
+    private static final File DEST_DIR = new File("Stage9/src/main/java/FolderCopy/primer/dst_dir");
+
+    public static void main(String[] args) throws IOException {
+        try{
+            FileUtils.copyDirectory(SRC_DIR, DEST_DIR);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
