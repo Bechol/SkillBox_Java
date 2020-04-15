2b259475e8901b380d92c9430ac10073a28f6cfc
Oleg Bech <37021780+Bechol@users.noreply.github.com>
2020-04-15 23:40:49 +0300

    Added user registration page template.

--- Stage12/src/main/java/ToDoList/controllers/RegistrationController.java
+++ Stage12/src/main/java/ToDoList/controllers/RegistrationController.java
@@ -1,11 +1,14 @@
 package ToDoList.controllers;
 
+import ToDoList.config.security.PasswordConfig;
 import ToDoList.models.User;
 import ToDoList.repositories.RoleRepository;
 import ToDoList.repositories.UserRepository;
 import com.google.common.base.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,8 +26,12 @@ public class RegistrationController {
     @Autowired
     RoleRepository roleRepository;
 
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
     @GetMapping
-    public String getRegPage() {
+    public String getRegPage(Model model) {
+        model.addAttribute("newUser", new User());
         return "registration";
     }
 
@@ -35,11 +42,12 @@ public class RegistrationController {
             return "registration";
         }
 
-        if (!newUser.getPassword().equals(newUser.getConfirmNewPassword())) {
+        if (!newUser.getNewPassword().equals(newUser.getConfirmNewPassword())) {
             return "registration";
         }
 
         if (!userRepository.findUserByUsername(newUser.getUsername()).isPresent()) {
+            newUser.setPassword(passwordEncoder.encode(newUser.getNewPassword()));
             newUser.setRoles(Collections.singleton(roleRepository.findByName("ROLE_USER")));
             userRepository.save(newUser);
         }
