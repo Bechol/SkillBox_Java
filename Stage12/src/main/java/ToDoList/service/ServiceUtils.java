c724606cb0e1fe7cabb56c58605e46e9e5728ace
Oleg Bech <37021780+Bechol@users.noreply.github.com>
2020-04-23 18:52:49 +0300

    Added javadoc to service utils.

--- Stage12/src/main/java/ToDoList/service/ServiceUtils.java
+++ Stage12/src/main/java/ToDoList/service/ServiceUtils.java
@@ -6,12 +6,18 @@ import org.springframework.security.core.context.SecurityContextHolder;
 /**
  * Класс ServiceUtils.
  * Утилитный класс для сервисного слоя.
+ *
  * @author Oleg Bech.
  * @email oleg071984@gmail.com.
  */
 public class ServiceUtils {
-
-    private User getAuthenticatedUser() {
+    /**
+     * Метод getAuthenticatedUser.
+     * Возврат авторизованного пользователя.
+     *
+     * @return объект класса User.
+     */
+    public static User getAuthenticatedUser() {
         return (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     }
 }
