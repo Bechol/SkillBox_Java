203f995321c1cb408e8cab20a95af1b98580e07c
Oleg Bech <37021780+Bechol@users.noreply.github.com>
2020-05-12 12:07:33 +0300

    Added homework 13.3 task1

---
+++ Stage13/src/main/java/Task1/RedisStorage.java
@@ -0,0 +1,60 @@
+package Task1;
+
+import org.redisson.Redisson;
+import org.redisson.api.RSet;
+import org.redisson.api.RedissonClient;
+import org.redisson.client.RedisConnectionException;
+import org.redisson.config.Config;
+
+/**
+ * Класс RedisStorage.
+ * Работа с котейнером Redis.
+ * Домашняя работа 13.3.
+ * Задание 1.
+ * Создаём контейнер Redis командой docker run --rm --name skill-redis -p 6379:6379 -d redis.
+ * @author Oleg Bech
+ * @email oleg071984@gmail.com
+ * @see package-info.java
+ */
+public class RedisStorage {
+
+    private RedissonClient redissonClient;
+    private RSet<String> todoSet;
+
+    /**
+     * Метод init().
+     * Подключение к docker-контейнеру Redis.
+     */
+    public void init() {
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://127.0.0.1:6379");
+        try {
+            redissonClient = Redisson.create(config);
+        } catch (RedisConnectionException Exc) {
+            System.out.println("Failed to connect to Redis");
+            System.out.println(Exc.getMessage());
+        }
+        todoSet = redissonClient.getSet("todoSet");
+    }
+
+    /**
+     * Метод addNewTodo.
+     * Добавление нового дела.
+     * @param todo новое дело.
+     */
+    public void addNewTodo(String todo) {
+        todoSet.add(todo);
+        System.out.println("Added new todo: " + todo);
+    }
+
+    /**
+     * Метод doTodo.
+     * Выполнение нового дела путем удаления из контейнера Redis.
+     * @param todo дело.
+     */
+    public void doTodo(String todo) {
+        todoSet.remove(todo);
+        System.out.println(todo + " -> done 100%.");
+    }
+
+}
---
+++ Stage13/src/main/java/Task1/RunTodoTask.java
@@ -0,0 +1,54 @@
+package Task1;
+
+import org.redisson.api.RSet;
+import org.redisson.api.RedissonClient;
+
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+/**
+ * Класс RunTodoTask.
+ * Демонтрация выполнения задания 13.3.1.
+ * @author Oleg Bech.
+ * @email oleg071984@gmail.com
+ * @see package-info.java
+ */
+public class RunTodoTask {
+    //Коллекция запланированных дел
+    private static RSet<String> todoSet;
+    //Клиент для контейнера Redis
+    private RedissonClient redissonClient;
+
+    public static void main(String[] args) {
+        //подключаемся к нашему контейнеру Redis
+        RedisStorage rs = new RedisStorage();
+        rs.init();
+        //коллекция запланированных дел
+        Set<String> todos = new HashSet<>();
+        todos.add("buy milk");
+        todos.add("fill the car");
+        todos.add("feed the cat");
+        //добавляем все запланированные дела в Redis
+        System.out.println("Add new cases to the list:");
+        todos.forEach(todo -> {
+            rs.addNewTodo(todo);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        //выполняем дела, удаляя поочередно из сета Redis
+        System.out.println("Do things from the list:");
+        todos.forEach(todo -> {
+            rs.doTodo(todo);
+            try {
+                Thread.sleep(new Random().nextInt(2000));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+    }
+}
