79be8d9f8ff2b0b58b6aeea320a43c0f13846d39
Oleg Bech <37021780+Bechol@users.noreply.github.com>
2020-05-28 16:54:30 +0300

    14_7 concatenation

--- Stage14/src/main/java/Homework_14_7/Concatenation.java
+++ Stage14/src/main/java/Homework_14_7/Concatenation.java
@@ -1,16 +1,13 @@
+package Homework_14_7;
 
-public class Concatenation
-{
-    public static void main(String[] args)
-    {
+public class Concatenation {
+    public static void main(String[] args) {
         long start = System.currentTimeMillis();
-
-        String str = "";
-        for(int i = 0; i < 20_000; i++)
-        {
-            str += "some text some text some text";
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < 20_000; i++) {
+            builder.append("some text some text some text");
         }
-
+        System.out.println(builder.toString().length());
         System.out.println((System.currentTimeMillis() - start) + " ms");
     }
 }
