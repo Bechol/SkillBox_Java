1dfc682685096a2eec2b54f5936daeb26023fbe1
Oleg Bech <37021780+Bechol@users.noreply.github.com>
2020-05-28 17:07:19 +0300

    14_7 loader optimization from lesson

--- Stage14/src/main/java/Homework_14_7/Loader.java
+++ Stage14/src/main/java/Homework_14_7/Loader.java
@@ -1,43 +1,41 @@
-import java.io.FileOutputStream;
+package Homework_14_7;
 
-public class Loader
-{
-    public static void main(String[] args) throws Exception
-    {
+import java.io.PrintWriter;
+
+public class Loader {
+    public static void main(String[] args) throws Exception {
         long start = System.currentTimeMillis();
 
-        FileOutputStream writer = new FileOutputStream("res/numbers.txt");
+        PrintWriter writer = new PrintWriter("res/numbers.txt");
 
         char letters[] = {'У', 'К', 'Е', 'Н', 'Х', 'В', 'А', 'Р', 'О', 'С', 'М', 'Т'};
-        for(int number = 1; number < 1000; number++)
-        {
-            int regionCode = 199;
-            for (char firstLetter : letters)
-            {
-                for (char secondLetter : letters)
-                {
-                    for (char thirdLetter : letters)
-                    {
-                        String carNumber = firstLetter + padNumber(number, 3) +
-                            secondLetter + thirdLetter + padNumber(regionCode, 2);
-                        writer.write(carNumber.getBytes());
-                        writer.write('\n');
+        for (int regionCode = 1; regionCode < 100; regionCode++) {
+            StringBuilder builder = new StringBuilder();
+            for (int number = 1; number < 1000; number++) {
+                for (char firstLetter : letters) {
+                    for (char secondLetter : letters) {
+                        for (char thirdLetter : letters) {
+                            builder.append(firstLetter);
+                            builder.append(padNumber(number, 3));
+                            builder.append(secondLetter);
+                            builder.append(thirdLetter);
+                            builder.append(padNumber(regionCode, 2));
+                            builder.append("\n");
+                        }
                     }
                 }
             }
+            writer.write(builder.toString());
         }
-
         writer.flush();
         writer.close();
-
         System.out.println((System.currentTimeMillis() - start) + " ms");
     }
 
-    private static String padNumber(int number, int numberLength)
-    {
+    private static String padNumber(int number, int numberLength) {
         String numberStr = Integer.toString(number);
         int padSize = numberLength - numberStr.length();
-        for(int i = 0; i < padSize; i++) {
+        for (int i = 0; i < padSize; i++) {
             numberStr = '0' + numberStr;
         }
         return numberStr;
