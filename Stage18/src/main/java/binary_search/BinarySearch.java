b5a6f4617a60acf1db313405778906e9351e828a
Oleg Bech <37021780+Bechol@users.noreply.github.com>
2020-06-17 00:00:09 +0300

    Stage18. Task3

--- Stage18/src/main/java/binary_search/BinarySearch.java
+++ Stage18/src/main/java/binary_search/BinarySearch.java
@@ -2,23 +2,29 @@ package binary_search;
 
 import java.util.ArrayList;
 
-public class BinarySearch
-{
+public class BinarySearch {
     private ArrayList<String> list;
 
-    public BinarySearch(ArrayList<String> list)
-    {
+    public BinarySearch(ArrayList<String> list) {
         this.list = list;
     }
 
-    public int search(String query)
-    {
+    public int search(String query) {
         return search(query, 0, list.size() - 1);
     }
 
-    private int search(String query, int from, int to)
-    {
-        //TODO: write code here
-        return -1;
+    private int search(String query, int from, int to) {
+        int middle = (from + to) / 2;
+        if (to < from) {
+            return -1;
+        }
+        int comparsion = query.compareTo(list.get(middle));
+        if (comparsion == 0) {
+            return middle;
+        }
+        if (comparsion > 0) {
+            return search(query, middle + 1, to);
+        }
+        return search(query, from, middle - 1);
     }
 }
